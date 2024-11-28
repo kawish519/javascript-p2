9164b590c7e06ae07ddbfa7ba1787b0659fdb341
kawish519 <9025234@student.zadkine.nl>
2024-11-28 14:18:25 +0100

    Add files via upload

---
+++ Opdracht 1/script.js
@@ -0,0 +1,8 @@
+// Functie om cookies te accepteren en de cookie muur te verbergen
+function acceptCookies() {
+    // Verberg de cookie muur
+    document.getElementById("cookie-wall").style.display = "none";
+
+    // Toon de hoofdcontent
+    document.getElementById("main-content").style.display = "block";
+}
