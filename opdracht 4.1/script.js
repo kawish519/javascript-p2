c706a7f2222c3ee49a7b067d211862132bf1d48f
kawish519 <9025234@student.zadkine.nl>
2024-12-16 11:57:24 +0100

    Add files via upload

---
+++ opdracht 4.1/script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('TafelVanTien');
+
+for (let i = 1; i <=10; i++) {
+
+const resultaat = i * 10;
+
+container.innerHTML += i + " x 10 = " + resultaat + "<br>"
+
+}
